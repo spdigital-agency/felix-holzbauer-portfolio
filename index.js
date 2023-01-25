fdd4bda1f80bfdad25eff2cb5d5c1f1e93a0b2ef
Raphael Peuckert <raphael@spdigital.agency>
2023-01-25 10:03:51 +0100

    Added Favicon, and implemented color change

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const darkToggleImg = document.querySelector('.dark-mode-toggle--img');
 let rootStyles = getComputedStyle(root);
 let darkMode = 'off';
 
+const favicon = document.querySelector("link[rel~='icon']");
+
 function darkToggle() {
     if(darkMode === 'off') {
         darkToggleImg.classList.add('dark-mode-on');
         root.style.setProperty('--primary', 'black');
         root.style.setProperty('--accent', 'white');
+        favicon.href = './images/icons/favicon-white.png'
 
         darkMode = 'on';
         console.log(darkMode);
@@ -15,6 +18,7 @@ function darkToggle() {
         darkToggleImg.classList.remove('dark-mode-on');
         root.style.setProperty('--primary', 'white');
         root.style.setProperty('--accent', 'black');
+        favicon.href = './images/icons/favicon-black.png';
 
         darkMode = 'off';
         console.log(darkMode);
